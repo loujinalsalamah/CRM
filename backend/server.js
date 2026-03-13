fcb22908844b823001f6657eb13f5aeaa575b0b6
loujin_alsalamah <loujinalsalamah@gmail.com>
2026-03-13 12:16:24 +0300

    init project

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+const prisma = require('./db');
+
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+async function connection() {
+  try {
+    await prisma.$queryRaw`SELECT 1`;
+    console.log('Database connected successfully!');
+  } catch (error) {
+    console.error('Database connection failed:', error);
+    process.exit(1);
+  }
+}
+
+connection();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
